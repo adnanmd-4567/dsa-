ac7b60f9c47875fa3a85ec533cee3481212b4264
Addy <adnanzaheeruddin1010@example.com>
2025-10-14 20:29:36 +0530

    Add second_largest.py

---
+++ second_largest.py
@@ -0,0 +1,24 @@
+class Solution:
+    def largest(self, arr):
+        temp = arr[0]
+        new = []
+        for i in range(len(arr)):
+            if arr[i] > temp:
+                temp = arr[i]
+            else:
+                new.append(arr[i])
+        
+        temp2 = new[0]
+    
+        if len(new) == 0:
+            return -1
+        
+        else:
+            for i in range(len(new)):
+                if new[i] > temp2:
+                    temp2 = new[i]
+            return temp2
+   
+arr = [12, 35, 1, 10, 34, 1]
+obj = Solution()       
+print(obj.largest(arr))  
