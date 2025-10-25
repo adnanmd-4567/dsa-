4467bf8aa9eb273300563527de4f21c2e71be2a8
adnanmd-4567 <155800525+adnanmd-4567@users.noreply.github.com>
2025-10-25 11:46:17 +0530

    Update type hint for list in missingNumber method

--- find_missing_number.py
+++ find_missing_number.py
@@ -1,7 +1,8 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
+    def missingNumber(self, nums: list[int]) -> int:
         nums.sort()
         for i in range(len(nums)):
             if i != nums[i]:
                 return i
         return i+1
+
