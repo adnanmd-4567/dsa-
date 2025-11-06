5abffb6e25731e38a8c814ed48e66b6f8cb95d7b
Addy <adnanzaheeruddin1010@example.com>
2025-11-06 21:23:45 +0530

    Add alternating_plus_and_minus.py

---
+++ alternating_plus_and_minus.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rearrangeArray(self, nums: list[int]) -> list[int]:
+        positive = []
+        negative = []
+        new = []
+        for num in nums:
+            if num < 0:
+                negative.append(num)
+            else:
+                positive.append(num)
+        
+        for i in range(len(negative)):
+            new.append(positive[i])
+            new.append(negative[i])
+        
+        return new
