5ea1bad5bd6611a33806b71b5dc791b62a27275a
Addy <adnanzaheeruddin1010@example.com>
2025-11-20 21:11:14 +0530

    add first_and_last_occurence.py

---
+++ first_and_last_occurence.py
@@ -0,0 +1,43 @@
+class Solution:
+    def searchRange(self, nums: list[int], target: int) -> list[int]:
+        def findFirst(nums,target):
+            left = 0
+            right = len(nums) - 1
+            first = -1
+
+            while left <= right:
+                mid = (left + right) // 2
+                
+                if nums[mid] == target:
+                    first = mid
+                    right = mid - 1 
+                
+                elif nums[mid] < target:
+                    left = mid + 1
+
+                else:
+                    right = mid - 1
+            return first
+
+        def findLast(nums, target):
+            left = 0
+            right = len(nums) - 1
+            last = -1
+
+            while left <= right:
+                mid = (left + right) // 2
+                
+                if nums[mid] == target:
+                    last = mid
+                    left = mid + 1 
+                
+                elif nums[mid] < target:
+                    left = mid + 1
+
+                else:
+                    right = mid - 1
+            return last
+
+        first = findFirst(nums, target)
+        last = findLast(nums, target)
+        return [first, last]
