6235bf593f2d47ea8625097dd30e7d228e35d850
Addy <adnanzaheeruddin1010@example.com>
2025-11-27 17:47:50 +0530

    Add num_of_rotations.py

---
+++ num_of_rotations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findKRotation(self, arr):
+        # code here
+        left = 0
+        right = len(arr) - 1
+        
+        while left < right:
+            mid = (left + right) // 2
+            
+            if arr[mid] > arr[right]:
+                left = mid + 1
+                
+            else:
+                right = mid
+        
+        return left
+        
