07a27f1599ea8426a916b2ecfed83b0cf68126f9
Addy <adnanzaheeruddin1010@example.com>
2025-11-29 18:06:57 +0530

    Add sqrt_of_num.py

---
+++ sqrt_of_num.py
@@ -0,0 +1,21 @@
+class Solution:
+    def floorSqrt(self, n): 
+        # code here
+        left = 0
+        right =  n
+        ans = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if mid * mid == n:
+                return mid
+                
+            elif mid * mid < n:
+                ans = mid
+                left = mid + 1
+                
+            else:
+                right = mid - 1
+                
+        return ans
